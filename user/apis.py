ec903629bce71592cbe81e5a53a224d320d1b9c6
WangNengZhi <2452384851@qq.com>
2020-02-22 15:14:34 +0800

    添加了api接口

--- user/apis.py
+++ user/apis.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_vcode(request):
+    """获取短信验证码"""
+    pass
+
+
+def submit_vcode(request):
+    """通过验证码登陆, 注册"""
+    pass
+
+
+def set_profile(request):
+    """修改个人资料"""
+    pass
+
+
+def upload_avatar(request):
+    """头像上传"""
+    pass
