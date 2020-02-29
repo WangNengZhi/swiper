26e5fac53fea6e519f431ea9706689e9023bf9a5
WangNengZhi <2452384851@qq.com>
2020-02-29 15:42:00 +0800

    创建了Swiped和Friend模型

--- social/apis.py
+++ social/apis.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from libs.http import render_json
+from social import logics
 
-# Create your views here.
+
+def rcmd_users(request):
+    """推荐用户接口"""
+    users = logics.rcmd(request.uid)
+    rcmd_data = [user.to_dict() for user in users]
+    return render_json(rcmd_data)
+
+
+def like(request):
+    """右滑喜欢"""
+    return render_json()
+
+
+def super_like(request):
+    """上滑超级喜欢"""
+    return render_json()
+
+
+def dislike(request):
+    """左滑不喜欢"""
+    return render_json()
+
+
+def rewind(request):
+    return render_json()
+
+
+def who_like_me(request):
+    return render_json()
+
+
+def friend_list(request):
+    return render_json()
--- social/models.py
+++ social/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Swiped(models.Model):
+    """滑动记录"""
+    STYPE = (
+        ('like', '上滑：喜欢'),
+        ('superlike', '上滑：超级喜欢'),
+        ('dislike', '左滑：不喜欢'),
+    )
+    uid = models.IntegerField(verbose_name='滑动者的ID')
+    sid = models.IntegerField(verbose_name='被滑动着的ID')
+    stype = models.CharField(max_length=16, choices=STYPE, verbose_name='滑动类型')
+    time = models.DateTimeField(auto_now_add=True, verbose_name='滑动时间')
+
+    class Meta:
+        db_table = 'swiped'
+
+
+class Friend(models.Model):
+    """好友关系"""
+    uid1 = models.IntegerField(verbose_name='好友ID')
+    uid2 = models.IntegerField(verbose_name='好友ID')
+
+    @classmethod
+    def make_friend(cls, uid1, uid2):
+        """创建好友关系"""
+        uid1, uid2 = (uid2, uid1) if uid1 > uid2 else (uid1, uid2)
+        cls.objects.create(uid1=uid1, uid2=uid2)
